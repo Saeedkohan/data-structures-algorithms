62b36d892f12037c753f9f661329fd259b5a675c
saeedkohansal <160484238+Saeedkohan@users.noreply.github.com>
2025-11-29 00:49:59 +0330

    add linear search and gitignore

---
+++ 04-Search/Linear Search/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+int linearSearch(std::vector<int> &arr, int target);
+
+int main() {
+    std::vector<int> arr = {-1, 2, 4, 5, 6, 78, 94};
+    int target = 78;
+    int result = linearSearch(arr, target);
+    if (result == -1) {
+        std::cout << "Element not found" << std::endl;
+    } else {
+        std::cout << "Element is present at index " << result;
+    }
+    return 0;
+}
+
+
+int linearSearch(std::vector<int> &arr, int target) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
